595fa67035acbbee36e4142590a3740e37bb51fa
Mariana Simion <marianasimiondossantos@gmail.com>
2025-09-18 10:25:49 -0300

    Adiciona verificação de vitória do jogador X

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -1,11 +1,21 @@
 #include "catch_amalgamated.hpp"
 #include "velha.hpp"
 
-TEST_CASE("Testa velha", "[single-file]") {
+TEST_CASE("Jogo impossível", "[velha]") {
     int teste1[3][3] = {
         { 1, 1, 1 },
         { 1, 1, 1 },
         { 1, 1, 1 }
     };
     REQUIRE(VerificaVelha(teste1) == -2);
+}
+
+
+TEST_CASE("Vitória jogador X", "[velha]") {
+    int teste2[3][3] = {
+        { 1, 1, 1 },
+        { 0, 2, 2 },
+        { 2, 0, 1 }
+    };
+    REQUIRE(VerificaVelha(teste2) == -2);
 }
